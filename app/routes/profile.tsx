99ba92d26186160bb8f79404ed22aee644d6a262
DNI9 <itsr9boi@gmail.com>
2022-02-11 00:22:57 +0530

    add profile page

--- app/routes/profile.tsx
+++ app/routes/profile.tsx
@@ -1,47 +1,58 @@
-import { User } from "@supabase/supabase-js";
-import { LoaderFunction, MetaFunction, useLoaderData } from "remix";
-import { getLoggedInUser, requireUserAccess } from "~/sessions.server";
-import { supabase } from "~/supabase";
+import {LogoutIcon} from '@heroicons/react/outline'
+import {User} from '@supabase/supabase-js'
+import {LoaderFunction, MetaFunction, useLoaderData} from 'remix'
+import {getLoggedInUser, requireUserAccess} from '~/sessions.server'
+import {supabase} from '~/supabase'
 
 type LoaderData = {
-  user: User | null;
-};
+  user: User | null
+}
 
-export const meta: MetaFunction = ({ data }: { data: LoaderData }) => {
-  const user = data?.user?.user_metadata;
+export const meta: MetaFunction = ({data}: {data: LoaderData}) => {
+  const user = data?.user?.user_metadata
   return {
-    title: `${user?.full_name ?? "Anon"} | Profile`,
-  };
-};
+    title: `${user?.full_name ?? 'Anon'} | Profile`,
+  }
+}
 
-export const loader: LoaderFunction = async ({ request }) => {
-  await requireUserAccess(request);
-  const user = await getLoggedInUser(request);
-  const data: LoaderData = { user };
-  return data;
-};
+export const loader: LoaderFunction = async ({request}) => {
+  await requireUserAccess(request)
+  const user = await getLoggedInUser(request)
+  const data: LoaderData = {user}
+  return data
+}
 
 export default function Profile() {
-  const data = useLoaderData<LoaderData>();
-  const user = data?.user?.user_metadata;
+  const data = useLoaderData<LoaderData>()
+  const user = data?.user?.user_metadata
 
-  if (!user) return <p>No user data available</p>;
+  if (!user) return <p>No user data available</p>
 
   return (
-    <div className="flex flex-col justify-center items-center mt-36">
-      <div className="w-28 h-28 bg-gray-700 p-2 mb-3 rounded-lg">
+    <main className='flex flex-col items-center mt-10'>
+      <div className='w-24 mb-3 rounded-lg bg-black2 h-w-24'>
         <img
-          className="rounded-lg"
+          className='w-full rounded-lg'
           src={user.picture}
           alt={`Photo of ${user.full_name}`}
         />
       </div>
-      <h1 className="text-5xl font-bold mb-2 text-center">{user.full_name}</h1>
-      <p>{user.email}</p>
-      <button className="mt-3" onClick={() => supabase.auth.signOut()}>
-        Sign out
+      <h1 className='text-4xl text-white'>{user?.full_name || 'Hello Anon'}</h1>
+      <button
+        onClick={() => supabase.auth.signOut()}
+        className='flex px-3 py-2 mt-8 space-x-2 font-medium text-black rounded-md bg-red active:scale-95'
+      >
+        <LogoutIcon className='w-6 h-6' />
+        <p>Logout</p>
       </button>
-      <p></p>
-    </div>
-  );
+      <p className='fixed text-white opacity-80 bottom-2'>
+        Made with 💙 by{' '}
+        <a href='https://github.com/dni9' target={'_blank'} rel='noreferrer'>
+          <span className='font-medium tracking-wide cursor-pointer text-blue'>
+            DNI9
+          </span>
+        </a>
+      </p>
+    </main>
+  )
 }
