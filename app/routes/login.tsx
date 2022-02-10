215c7114ed9366b8bb449e900a60a0696e8b23b0
DNI9 <itsr9boi@gmail.com>
2022-02-11 00:28:22 +0530

    add login page

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,3 +1,4 @@
+import {LoginIcon} from '@heroicons/react/outline'
 import {MetaFunction} from 'remix'
 import {supabase} from '~/supabase'
 
@@ -13,13 +14,35 @@ export default function LogIn() {
   }
 
   return (
-    <div className='flex flex-col items-center justify-center mt-36'>
-      <h1 className='mb-2 text-5xl font-bold text-center'>
-        Log in to continue
-      </h1>
-      <button onClick={handleGoogleSignIn} className='mt-5'>
-        Continue with Google
-      </button>
-    </div>
+    <main>
+      <div className='fixed top-0 -right-5'>
+        <svg
+          width='323'
+          height='411'
+          viewBox='0 0 323 411'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            fillRule='evenodd'
+            clipRule='evenodd'
+            d='M365.647 -94.0876C438.928 -73.0842 465.372 12.1468 500.201 79.9575C521.838 122.084 531.327 167.7 524.148 214.511C518.085 254.047 489.569 282.181 464.419 313.284C434.218 350.634 413.506 406.463 365.647 410.534C316.886 414.681 291.453 355.117 248.512 331.647C166.895 287.038 37.6428 302.332 7.00232 214.511C-23.7002 126.513 52.3283 32.629 122.975 -28.16C189.028 -84.9954 281.88 -118.096 365.647 -94.0876Z'
+            fill='#302D41'
+            fillOpacity='0.8'
+          />
+        </svg>
+      </div>
+      <div className='fixed bottom-20 left-5'>
+        <p className='text-lg text-blue opacity-80'>Create your own</p>
+        <h1 className='text-6xl font-semibold text-white'>Secret message</h1>
+        <button
+          onClick={handleGoogleSignIn}
+          className='flex px-3 py-2 mt-5 space-x-2 font-medium text-black rounded-md bg-blue active:scale-95'
+        >
+          <LoginIcon className='w-6 h-6' />
+          <p>Login with Google</p>
+        </button>
+      </div>
+    </main>
   )
 }
