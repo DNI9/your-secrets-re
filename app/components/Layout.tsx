72278b2d2a107e6e6ff924c9de89013876006460
DNI9 <itsr9boi@gmail.com>
2022-02-11 01:02:22 +0530

    hide nav on certain pages

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, {useCallback} from 'react'
+import {useLocation} from 'remix'
 import {Nav} from './Nav'
 
+const navExcludedPaths = ['/login']
+
 export default function Layout({children}: {children: React.ReactNode}) {
+  const location = useLocation()
+
+  const checkLocation = useCallback(
+    () => !navExcludedPaths.includes(location.pathname),
+    [location.pathname]
+  )
+
+  const showNav = checkLocation()
+
   return (
     <>
-      <Nav title='Secrets' />
+      {showNav ? <Nav title='Secrets' /> : null}
       <div className='container min-h-full px-4 md:mx-auto'>{children}</div>
     </>
   )
