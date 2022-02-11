e83aa8fe1aeb0ba08e5f8fcac0bd482c4237e77f
DNI9 <itsr9boi@gmail.com>
2022-02-11 11:57:34 +0530

    add new secret submission

--- app/routes/secrets/new.tsx
+++ app/routes/secrets/new.tsx
@@ -1,6 +1,79 @@
-import {Form} from 'remix'
+import {PostgrestError} from '@supabase/supabase-js'
+import {ActionFunction, Form, json, redirect, useActionData} from 'remix'
+import {getLoggedInUser} from '~/sessions.server'
+import {supabase} from '~/supabase'
+import {SecretType} from '~/types'
+
+type ActionData = {
+  formError?: string
+  fieldErrors?: {
+    secret: string | undefined
+  }
+  fields?: {
+    secret: string
+  }
+  submitError?: PostgrestError
+}
+
+const badRequest = (data: ActionData) => json(data, {status: 400})
+
+const validateSecretName = (secret: string) => {
+  if (secret.length < 4) return 'Name must be longer than 3 characters'
+}
+
+export const action: ActionFunction = async ({request}) => {
+  const user = await getLoggedInUser(request)
+  if (!user) throw redirect('/')
+  const form = await request.formData()
+  const secret = form.get('secret')
+
+  if (typeof secret !== 'string') {
+    return badRequest({formError: `Form not submitted correctly.`})
+  }
+  const fieldErrors = {
+    secret: validateSecretName(secret),
+  }
+
+  const fields = {secret}
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return badRequest({fieldErrors, fields})
+  }
+
+  const {error} = await supabase
+    .from<SecretType>('secrets')
+    .insert({secret_name: secret, created_by: user.id}, {returning: 'minimal'})
+
+  if (error) return badRequest({submitError: error})
+
+  return redirect('/')
+}
+
+const ErrorMessage = ({data}: {data?: ActionData}) => {
+  return (
+    <>
+      {data?.fieldErrors?.secret ? (
+        <p className='error-text' role='alert'>
+          {data.fieldErrors.secret}
+        </p>
+      ) : null}
+      {data?.formError ? (
+        <p className='error-text' role='alert'>
+          {data.formError}
+        </p>
+      ) : null}
+      {data?.submitError ? (
+        <p className='error-text' role='alert'>
+          oh no! could not submit form, try again (ErrorCode:{' '}
+          {data.submitError.code})
+        </p>
+      ) : null}
+    </>
+  )
+}
 
 export default function NewSecret() {
+  const actionData = useActionData<ActionData>()
+
   return (
     <main className='mt-5'>
       <p className='text-lg text-blue opacity-80'>Add a new</p>
@@ -9,9 +82,13 @@ export default function NewSecret() {
         <input
           type='text'
           name='secret'
+          autoFocus
           placeholder='type a name, e.g another secret'
+          defaultValue={actionData?.fields?.secret}
           className='w-full h-12 p-3 mt-3 text-white rounded-md outline-none bg-black2 focus:outline-blue'
         />
+        <ErrorMessage data={actionData} />
+
         <button
           type='submit'
           className='flex px-3 py-2 mt-5 font-medium text-black rounded-md bg-blue active:scale-95 disabled:bg-gray'
